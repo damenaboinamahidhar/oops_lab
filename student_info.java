bdb2f80f8f5604c6a610aaad78b2e2dd9d4daf93
damenaboinamahidhar <damenaboinamahidhar10@gmail.com>
2026-01-16 11:09:12 +0530

    Add files via upload

---
+++ student_info.java
@@ -0,0 +1,22 @@
+class student_info
+{
+public static void main(String args[])
+{
+String student_name="jai";
+int age=15;
+char gender='M';
+String class_studying="tenth";
+long hall_ticket=246679012L;
+int marks_secured=530;
+double percentage=(marks_secured/600.0)*100;
+boolean pass=true;
+System.out.println("student name:"+student_name);
+System.out.println("student age:"+age);
+System.out.println("gender:"+gender);
+System.out.println("Grade of study:"+class_studying);
+System.out.println("Hall ticket number:"+hall_ticket);
+System.out.println("marks secured :"+marks_secured);
+System.out.println("Percentage:"+percentage);
+System.out.println("IS Student passed :"+pass);
+}
+}
