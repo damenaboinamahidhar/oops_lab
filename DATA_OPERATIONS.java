a73fb44016b96c99e3b88d4f2cf39dd4f508a8a2
damenaboinamahidhar <damenaboinamahidhar10@gmail.com>
2026-01-19 17:40:35 +0530

    Add files via upload

---
+++ DATA_OPERATIONS.java
@@ -0,0 +1,26 @@
+class DATA_OPERATIONS
+{
+public static void main(String args[])
+{
+int a=100;
+int b=5;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a/b);
+System.out.println(a%b);
+a++;
+System.out.println(a);
+b--;
+System.out.println(b);
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a<=b);
+System.out.println(a>=b);
+System.out.println(a!=b);
+System.out.println(a==b);
+System.out.println(a>50 && b<50);
+System.out.println(a>120 || b<120);
+System.out.println(!(a==b));
+}
+}
+
